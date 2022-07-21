52c2395104777e861173f1ff64e1868ed7b53de9
miss-f-g <fatemeghafari98@gmail.com>
2022-07-21 20:11:44 +0430

    13.create CardEL comp

--- src/components/bolg/Blogs.js
+++ src/components/bolg/Blogs.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useQuery } from "@apollo/client/react";
 import { GET_BLOGS_INFO } from "../../graphql/queries";
+import { Grid } from "@mui/material";
+import CardEL from "../shared/CardEL";
 
 const Blogs = () => {
   const { loading, data, errors } = useQuery(GET_BLOGS_INFO);
@@ -8,7 +10,15 @@ const Blogs = () => {
 
   if (loading) return <h4>Loading...</h4>;
   if (errors) return <h4>Error ...</h4>;
-  return <div>Blogs</div>;
+  return (
+    <Grid container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid item xs={12} sm={6} md={4} key={post.id}>
+          <CardEL {...post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Blogs;
---
+++ src/components/shared/CardEL.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardEL = (props) => {
+  return (
+    <div>CardEL</div>
+  )
+}
+
+export default CardEL
