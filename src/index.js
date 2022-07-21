b316a79c2658f845a37609879a562ce1f4a46d52
miss-f-g <fatemeghafari98@gmail.com>
2022-07-21 17:30:34 +0430

    7.add env

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ import App from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import theme from "./mui/theme";
 
+
+
 const client = new ApolloClient({
-  uri: "https://api-us-east-1.hygraph.com/v2/cl5rwt7y10khr01ukc81y0y22/master",
+  uri: process.env.REACT_APP_GRAPH_URI ,
   cache: new InMemoryCache(),
 });
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <ApolloProvider client={client}>
     <ThemeProvider theme={theme}>
