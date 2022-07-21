505b8ce4e030743b28028d1ebaa0cdc3ff5a0893
miss-f-g <fatemeghafari98@gmail.com>
2022-07-21 15:51:36 +0430

    3.install mui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
 import "./App.css";
 
-const QUERY = gql`
-  query {
-    authors {name}
-  }
-`;
+
 
 function App() {
-  const { loading, data } = useQuery(QUERY);
-  console.log(data)
+
   return <div className="App">hi</div>;
 }
 
