1079c18ba614a55e7ad96f02b2d6a758ba4c3eba
miss-f-g <fatemeghafari98@gmail.com>
2022-07-21 18:34:57 +0430

    11.fix some issues

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <foo
-  )
-}
+    <footer>
+      <Typography
+        component="p"
+        variant="p"
+        bgcolor="#f7f7f7"
+        color="primary"
+        padding="10px"
+        textAlign="center"
+        mt={10}
+      >
+        پروژه وبلاگ با GraphQL |  سایت mfgh.ir
+      </Typography>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
