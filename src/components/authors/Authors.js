e851124e41c7913328fcde50710e10b3bdb782c2
miss-f-g <fatemeghafari98@gmail.com>
2022-07-21 20:49:36 +0430

    15.grt Authors data

--- src/components/authors/Authors.js
+++ src/components/authors/Authors.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { GET_AUTHORS_INFO } from '../../graphql/queries';
+import { useQuery } from "@apollo/client";
 
 const Authors = () => {
+    const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+
+    
+  if (loading) return <h3>Loading ...</h3>
+
+  if (errors) return <h3>Error...</h3>;
+
+  console.log(data)
   return (
     <div>Authors</div>
   )
