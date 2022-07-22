3e025e3ae881dce2691995abc869d45f36012d75
miss-f-g <fatemeghafari98@gmail.com>
2022-07-22 12:01:36 +0430

    21.add GET_POST_INFO

--- src/components/bolg/BlogPage.js
+++ src/components/bolg/BlogPage.js
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useQuery } from '@apollo/client';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { GET_POST_INFO } from '../../graphql/queries';
+import Loader from '../shared/Loader';
 
 const BlogPage = () => {
+
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  const { loading, data, errors } = useQuery(GET_POST_INFO, {
+    variables: { slug },
+  });
+
+
+  if (loading) return <Loader />;
+  if (errors) return <h3>Error...</h3>;
+
+  
   return (
     <div>BlogPage</div>
   )
