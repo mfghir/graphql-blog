9f8c62c6e2d7328714523af87c1c4c95adb195a1
miss-f-g <fatemeghafari98@gmail.com>
2022-07-22 10:18:43 +0430

    16.add style to Authors comp

--- src/components/authors/Authors.js
+++ src/components/authors/Authors.js
@@ -1,19 +1,52 @@
-import React from 'react'
-import { GET_AUTHORS_INFO } from '../../graphql/queries';
+import React from "react";
+import { GET_AUTHORS_INFO } from "../../graphql/queries";
 import { useQuery } from "@apollo/client";
 
-const Authors = () => {
-    const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+import { Avatar, Divider, Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
-    
-  if (loading) return <h3>Loading ...</h3>
+const Authors = () => {
+  const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+
+  if (loading) return <h3>Loading ...</h3>;
 
   if (errors) return <h3>Error...</h3>;
 
-  console.log(data)
+  console.log(data);
+  const { authors } = data;
   return (
-    <div>Authors</div>
-  )
-}
+    <Grid
+      container
+      sx={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}
+    >
+      {authors.map((author, index) => (
+        <div key={author.id}>
+          <Grid item xs={12} padding={2}>
+            <Link
+              to={`/authors/${author.slug}`}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                textDecoration: "none",
+              }}
+            >
+              <Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />
 
-export default Authors
+              <Typography component="p" variant="p" color="text.secondary">
+                {author.name}
+              </Typography>
+            </Link>
+          </Grid>
+
+          {index !== authors.length - 1 && (
+            <Grid item xs={12}>
+              <Divider variant="middle" />
+            </Grid>
+          )}
+        </div>
+      ))}
+    </Grid>
+  );
+};
+
+export default Authors;
